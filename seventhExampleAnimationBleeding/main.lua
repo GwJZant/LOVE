71307597a3e3c705cb717a79e965b4940f289b8c
GwJZant <102416279+GwJZant@users.noreply.github.com>
2025-10-13 18:31:56 -0400

    seventhExampleAnimationBleeding
    example of making a quad and trimming off border pixels

--- seventhExampleAnimationBleeding/main.lua
+++ seventhExampleAnimationBleeding/main.lua
@@ -1,18 +1,22 @@
 function love.load()
-  image = love.graphics.newImage("jump.png")
+  image = love.graphics.newImage("jump_3.png")
+  local width = image:getWidth()
+  local height = image:getHeight() 
   frames = {}
   local frame_width = 117
   local frame_height = 233
   
-  for i=0,4 do
-    table.insert(frames, love.graphics.newQuad(i * frame_width, 0, frame_width, frame_height, image:getWidth(), image:getHeight()))
+  for i=0,1 do
+    for j=0,2 do
+      table.insert(frames, love.graphics.newQuad(1 + j * (frame_width + 2), 1 + i * (frame_height + 2), frame_width, frame_height, width, height))
+    end
   end
   
   currentFrame = 1
 end
 
 function love.update(dt)
-  currentFrame = currentFrame + dt
+  currentFrame = currentFrame + 10 * dt
   
   if currentFrame >= 6 then
     currentFrame = 1
